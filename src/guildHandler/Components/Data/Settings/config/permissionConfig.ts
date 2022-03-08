3e5bdf2717105b26988d3383fdc95e00d18021cb
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2022-03-08 16:45:30 -0600

    refactor: Fix some capitalization

--- src/guildHandler/Components/Data/Settings/config/permissionConfig.ts
+++
@@ -1,8 +0,0 @@
-// Permissions Configuration data and defaults
-export type PermissionsConfig = {
-	[key: string]: Array<string>;
-};
-export const PERMISSIONS_DEFAULT = {
-	everyone: ['join', 'play', 'pause', 'resume', 'stop'],
-	admin: ['set-channel']
-};
