56a7aff4c1943df1bc870d2e680ed0759c2a19d2
Bennett Wu <57691028+bwu1324@users.noreply.github.com>
2023-03-13 16:21:34 -0500

    feat(music_cache): finalize music_cache interface

--- src/music_cache/music_cache.ts
+++ src/music_cache/music_cache.ts
@@ -3,18 +3,45 @@ import Logger from '../logger/logger';
 export enum MusicCacheFunctions {
   cache,
   cacheLocation,
+  releaseLock,
 }
 
+/**
+ * MusicCache - Manages the downloaded music in a cache on the disk limited to a given size
+ * Lifecycle of a song:
+ * - Call cache() to begin download of song to cache (or keep song in cache if already cached)
+ * - Call cacheLocation() to get the location of the cached song
+ * - Call releaseLock() to release the playback lock on the song so that it may be deleted if space is needed
+ */
 export default class MusicCache {
   constructor(logger: Logger) {
     //
   }
 
-  cache(url: string): void {
+  /**
+   * cache() - Caches a song in the music cache
+   * @param url - url of song to cache
+   * @returns - cache lock_id for the song (must be unlocked after song has been played)
+   */
+  cache(url: string): Promise<number> {
     return;
   }
 
+  /**
+   * cacheLocation() - Fetches the cache location information of a song
+   * @param url - url of song to fetch cache location of
+   * @returns - object containing cache location information
+   */
   cacheLocation(url: string): { uid: string; loc: string; end_chunk: number } {
     return { uid: '', loc: '', end_chunk: 0 };
+  }
+
+  /**
+   * releaseLock() - Release the lock on a song
+   * @param lock_id - cache lock_id for the song (recieved from cache() function)
+   * @returns - Promise that resolves once lock has been successfully released
+   */
+  releaseLock(lock_id: number): Promise<void> {
+    return;
   }
 }
--- src/music_cache/music_cache_ipc_interface.ts
+++ src/music_cache/music_cache_ipc_interface.ts
@@ -4,16 +4,38 @@ import Logger from '../logger/logger';
 import IPCInterface from '../ipc_template/ipc_interface';
 import { MusicCacheFunctions } from './music_cache';
 
+/**
+ * MusicCacheInterface - Connects to a MusicCacheInterface component running as a seperate thread
+ */
 export default class MusicCacheInterface extends IPCInterface<MusicCacheFunctions> {
   constructor(logger: Logger) {
     super(ipc_config.music_ipc_id, logger);
   }
 
-  async cache(url: string): Promise<void> {
-    await this.RequestFunction(MusicCacheFunctions.cache, [url]);
+  /**
+   * cache() - Caches a song in the music cache
+   * @param url - url of song to cache
+   * @returns - cache lock_id for the song (must be unlocked after song has been played)
+   */
+  async cache(url: string): Promise<number> {
+    return parseInt(await this.RequestFunction(MusicCacheFunctions.cache, [url]));
   }
 
-  async cacheLocation(url: string): Promise<{ uid: string; loc: string; end_chunk: number }> {
+  /**
+   * cacheLocation() - Fetches the cache location information of a song
+   * @param url - url of song to fetch cache location of
+   * @returns - object containing cache location information
+   */
+  async cacheLocation(url: string): Promise<{ cache_location: string; start_chunk: number; end_chunk: number }> {
     return JSON.parse(await this.RequestFunction(MusicCacheFunctions.cacheLocation, [url]));
+  }
+
+  /**
+   * releaseLock() - Release the lock on a song
+   * @param lock_id - cache lock_id for the song (recieved from cache() function)
+   * @returns - Promise that resolves once lock has been successfully released
+   */
+  async releaseLock(lock_id: number): Promise<void> {
+    await this.RequestFunction(MusicCacheFunctions.releaseLock, [lock_id.toString()]);
   }
 }
